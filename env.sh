a9b4949de84a93168072e73d898d90e05fa2f5b2
Subash Niroula <25361033+subash1999@users.noreply.github.com>
2025-03-29 19:35:33 +0900

    rider service added

--- env.sh
+++ env.sh
@@ -3,7 +3,7 @@ GITHUB_HOST="github.com"
 GITHUB_USERNAME="nepal-ride-sharing-app"
 MOBILE_APP_REPOS=("mobile-app-driver" "mobile-app-rider")
 LIBRARIES_REPOS=("common")
-SERVICES_REPOS=("driver-service" )
+SERVICES_REPOS=("driver-service", "rider-service" )
 
 NODE_MAJOR_VERSION_REQUIRED=18
 
